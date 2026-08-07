8318cc8921d44574bd98a11c3b3c15312b4c32d9
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-07 15:58:39 +0530

    Time: 19 ms (83.69%), Space: 45.8 MB (47.08%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,24 +1,27 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
+        int n = nums.length;
 
         for(int i = 0; i < n; i++){
             if(i > 0 && nums[i] == nums[i - 1]) continue;
-            for(int j = i + 1; j < n; j++){
+            for(int j = i + 1; j < n;j++){
                 if(j > i + 1 && nums[j] == nums[j - 1]) continue;
                 int k = j + 1;
                 int l = n - 1;
                 while(k < l){
-                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
-
+                    long sum =  ( long) nums[i] + nums[j] + nums[k] + nums[l];
                     if(sum < target){
                         k++;
-                    }else if (sum > target){
-                        l --;
+                    }else if(sum > target){
+                        l--;
                     }else{
-                        List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add(nums[l]);
                         ans.add(temp);
                         k++;
                         l--;
