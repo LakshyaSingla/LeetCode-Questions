4265188dab11c7d40319ddbc411fde158dad06bf
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-12 22:03:13 +0530

    Time: 3 ms (72.5%), Space: 52.8 MB (29.82%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,10 +1,9 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length;
-        int c1 = 0, c2 = 0;
         int ele1 = Integer.MIN_VALUE, ele2 = Integer.MIN_VALUE;
+        int c1 = 0, c2 = 0;
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < nums.length; i++){
             if(nums[i] == ele1){
                 c1++;
             }else if(nums[i] == ele2){
@@ -13,25 +12,27 @@ class Solution {
                 ele1 = nums[i];
                 c1++;
             }else if(c2 == 0){
-                 ele2 = nums[i];
+                ele2= nums[i];
                 c2++;
             }else{
                 c1--;
                 c2--;
             }
         }
-        c1=0;
-        c2=0;
-        int mini = (n / 3) + 1;
-        for(int i = 0; i < n; i++){
-            if(nums[i] == ele1) c1++;
-            if(nums[i] == ele2) c2++;
+        c1 = 0;
+        c2 = 0;
+        for(int num : nums){
+            if(num == ele1) c1++;
+            if(num == ele2)c2++;
         }
+        int mini = nums.length / 3 + 1;
         List<Integer> ans = new ArrayList<>();
-        if(c1 >= mini) ans.add(ele1);
-        if(c2 >= mini) ans.add(ele2);
+        if(c1 >= mini){
+            ans.add(ele1);
+        }
+        if(c2 >= mini){
+            ans.add(ele2);
+        }
         return ans;
-        
-
     }
 }
