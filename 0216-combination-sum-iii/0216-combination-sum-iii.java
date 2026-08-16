a7d28dbaf44925f922eb5cb0e2d0477cba62dcd7
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-16 23:02:36 +0530

    Time: 0 ms (100%), Space: 42.4 MB (66.26%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,22 +1,21 @@
 class Solution {
-    void backtrack(int n, int k, List<List<Integer>> ans, List<Integer> curr){
+    void backtrack(int start, int k, int n, List<List<Integer>> ans,List<Integer> curr){
         if(n == 0 && curr.size() == k){
             ans.add(new ArrayList<>(curr));
             return;
         }
         if(n < 0 || curr.size() > k) return;
-        int ele = (curr.isEmpty()) ? 1 : curr.get(curr.size() - 1) + 1;
-        for(int i = ele; i <= 9; i++){
+        for(int i = start; i <= 9; i++){
             if(i > n) break;
             curr.add(i);
-            backtrack(n - i, k, ans, curr);
+            backtrack(i + 1, k, n - i, ans, curr);
             curr.remove(curr.size() - 1);
         }
     }
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
-        backtrack(n, k, ans, curr);
+        backtrack(1, k, n, ans,curr);
         return ans;
     }
 }
