da5a7838c28b5ec4ea35f2354f994cfb3e0fa6e3
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-16 22:42:35 +0530

    Time: 8 ms (15.56%), Space: 48.2 MB (9.95%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,6 +1,5 @@
 class Solution {
-    void backtrack(int index, int n, int[] candidates, int target,  List<List<Integer>> ans,List<Integer> curr){
-
+    void backtrack(int index, int n, List<Integer> curr, List<List<Integer>> ans,int[] candidates, int target){
         if(target == 0){
             ans.add(new ArrayList<>(curr));
             return;
@@ -8,21 +7,21 @@ class Solution {
         if(target < 0 || index == n) return;
 
         curr.add(candidates[index]);
-        backtrack(index + 1, n, candidates, target - candidates[index], ans, curr);
+        backtrack(index + 1, n, curr, ans, candidates, target - candidates[index]);
         curr.remove(curr.size() - 1);
         for(int i = index + 1; i < n; i++){
             if(candidates[i] != candidates[index]){
-                backtrack(i, n, candidates, target, ans, curr);
+                backtrack(i, n, curr,ans, candidates, target);
                 break;
             }
         }
-        
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> curr = new ArrayList<>();
         Arrays.sort(candidates);
-        backtrack(0, candidates.length, candidates, target, ans, curr);
+        int n = candidates.length;
+        List<Integer> curr = new ArrayList<>();
+        backtrack(0, n, curr, ans, candidates, target);
         return ans;
     }
 }
