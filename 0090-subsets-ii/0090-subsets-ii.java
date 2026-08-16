0d3a2c4717b6715fad1a3cc1856da1dd24ea9ee8
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-16 22:53:01 +0530

    Time: 2 ms (99.74%), Space: 44.9 MB (77.18%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,26 +1,28 @@
 class Solution {
-    void backtrack(int index, int[] nums, int n,List<List<Integer>> ans, List<Integer> curr){
+    void backtrack(int index, int[] nums, List<List<Integer>> ans, List<Integer> curr, int n){
+
         if(index == n){
             ans.add(new ArrayList<>(curr));
             return;
-        }
+        }        
         curr.add(nums[index]);
-        backtrack(index + 1, nums, n, ans, curr);
+        backtrack(index + 1, nums, ans, curr,n);
         curr.remove(curr.size() - 1);
         for(int i = index + 1; i < n; i++){
             if(nums[i] != nums[index]){
-                backtrack(i, nums, n, ans, curr);
+                backtrack(i, nums, ans, curr, n);
                 return;
             }
         }
-        backtrack(n, nums, n, ans, curr);
+        backtrack(n, nums, ans, curr, n);
 
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
+        int n = nums.length;
         Arrays.sort(nums);
-        backtrack(0, nums, nums.length, ans, curr);
+        backtrack(0, nums, ans, curr, n);
         return ans;
     }
 }
