93709dc6e7b00a5371e891e1dbead97e659acd54
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-17 20:51:42 +0530

    Time: 0 ms (100%), Space: 47.7 MB (71.68%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,17 +1,16 @@
 class Solution {
     public double myPow(double x, int n) {
         if(n == 0) return 1.0;
-        if(n == 1) return x;
         double num = n;
         if(num < 0){
-            x = 1/x;
             num = -num;
+            x = 1/x;
         }
+
         if(num % 2 == 0){
-            return myPow(x * x,(int) num / 2);
+         return myPow(x * x, (int) num / 2);
         }
             return x * myPow(x, (int) num - 1);
-       
+        
     }
-    
 }
