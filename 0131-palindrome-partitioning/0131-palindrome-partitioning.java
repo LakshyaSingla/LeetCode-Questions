41d0208dd904fa21ffb9b44472cc4c14d4c85afe
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-21 23:48:28 +0530

    Time: 7 ms (96.93%), Space: 65 MB (87.88%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,27 +1,26 @@
 class Solution {
-    void search(int index, String s, List<List<String>> ans,List<String> curr){
+    void backtrack(int index, String s, List<List<String>> ans, List<String> curr){
         if(index == s.length()){
             ans.add(new ArrayList<>(curr));
             return;
         }
-
         for(int i = index; i < s.length(); i++){
             if(isPalindrome(s, index, i)){
-            curr.add(s.substring(index, i + 1));
-            search(i + 1, s, ans, curr);
-            curr.remove(curr.size() - 1);
-        }
+                curr.add(s.substring(index, i + 1));
+                backtrack(i + 1, s, ans, curr);
+                curr.remove(curr.size() - 1);
+            }
         }
     }
     public List<List<String>> partition(String s) {
         List<List<String>> ans = new ArrayList<>();
         List<String> curr = new ArrayList<>();
-        search(0, s, ans, curr);
+        backtrack(0,s,ans,curr);
         return ans;
     }
-    boolean isPalindrome(String s, int first, int last){
-        while(first < last){
-            if(s.charAt(first++) != s.charAt(last--)){
+    boolean isPalindrome(String s, int index, int i){
+        while(index < i){
+            if(s.charAt(index++)!=s.charAt(i--)){
                 return false;
             }
         }
