f166fea4644eff5f088087be983f5302bf530d2a
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-21 23:34:07 +0530

    Time: 0 ms (100%), Space: 43.4 MB (64.84%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,24 +1,26 @@
 class Solution {
     private final static String[] MAP = {
-        "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"
+        "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
     };
-    void backtrack(int index,List<String> ans, String digits, StringBuilder curr){
 
+    void backtrack(int index, String digits, List<String> ans, StringBuilder curr){
         if(index == digits.length()){
             ans.add(curr.toString());
             return;
         }
+
+
         int digit = digits.charAt(index) - '0';
-        String letter = MAP[digit];
-        for(int i = 0; i < letter.length(); i++){
-            curr.append(letter.charAt(i));
-            backtrack(index + 1, ans, digits, curr);
+        String letters = MAP[digit];
+        for(int i = 0; i < letters.length(); i++){
+            curr.append(letters.charAt(i));
+            backtrack(index + 1, digits, ans, curr);
             curr.deleteCharAt(curr.length() - 1);
         }
     }
     public List<String> letterCombinations(String digits) {
         List<String> ans = new ArrayList<>();
-        backtrack(0, ans, digits, new StringBuilder());
+        backtrack(0, digits, ans, new StringBuilder());
         return ans;
     }
 }
