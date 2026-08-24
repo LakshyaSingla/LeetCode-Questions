0ef3c1a81bf591820e2477ade63a094235f5bd11
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-24 21:32:45 +0530

    Time: 14 ms (7.02%), Space: 43.7 MB (5.02%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,39 +1,8 @@
 class Solution {
-    int Queen(int row, int n, List<String> board){
-        int count = 0;
-        if(row == n){
-            return 1;
-        }
-
-        for(int col = 0; col < n; col++){
-            if(isValid(row, col,board)){
-                char[] arr = board.get(row).toCharArray();
-                arr[col] = 'Q';
-                board.set(row, new String(arr));
-                count+= Queen(row + 1, n, board);
-                arr[col] = '.';
-                board.set(row, new String(arr));
-            }
-        }
-        return count;
-    }
-    public int totalNQueens(int n) {
-        List<String> board = new ArrayList<>();
-
-        String temp = "";
-        for(int i = 0; i < n; i++){
-            temp+=".";
-        }
-        for(int i = 0; i < n; i++){
-            board.add(temp);
-        }
-        return Queen(0,n,board);
-         
-    }
-    boolean isValid(int row, int col, List<String> board){
-
+     boolean isValid(int row, int col, List<String> board){
         int r = row;
         int c = col;
+
         while(r >= 0){
             if(board.get(r).charAt(c) == 'Q'){
                 return false;
@@ -41,16 +10,17 @@ class Solution {
             r--;
         }
         r = row;
-        while(r >= 0 && c >= 0){
+        while(r >=0 && c >= 0){
             if(board.get(r).charAt(c) == 'Q'){
                 return false;
             }
             r--;
             c--;
+
         }
-        r = row; 
+        r = row;
         c = col;
-        while(r >= 0 && c < board.size()){
+        while(r >= 0 && c < board.get(0).length()){
             if(board.get(r).charAt(c) == 'Q'){
                 return false;
             }
@@ -58,5 +28,36 @@ class Solution {
             c++;
         }
         return true;
+    }
+    int FindBoard(int row,List<String> board, List<List<String>> ans){
+        if(row == board.size()){
+            ans.add(new ArrayList<>(board));
+            return 1;
+        }
+        int count = 0;
+        for(int col = 0; col < board.get(0).length(); col++){
+            if(isValid(row, col, board)){
+                char[] arr = board.get(row).toCharArray();
+                arr[col] = 'Q';
+                board.set(row, new String(arr));
+                count += FindBoard(row + 1, board,ans);
+                arr[col] = '.';
+                board.set(row, new String(arr));
+            }
+        }
+        return count;
+    }
+    public int totalNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
+        List<String> board = new ArrayList<>();
+        String temp = "";
+        for(int i = 0; i < n; i++){
+            temp+= '.';
+        }
+        for(int i = 0; i < n; i++){
+            board.add(temp);
+        }
+        return FindBoard(0, board, ans);
+        
     }
 }
