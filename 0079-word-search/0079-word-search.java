b5e57123474b096781afa38a76441ea0a335da3a
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-24 18:40:53 +0530

    Time: 136 ms (52.63%), Space: 43.1 MB (43.66%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,25 +1,23 @@
 class Solution {
-    boolean Func(int i, int j, char[][] board, String word, int index){
+    boolean WordFinder(int i, int j, char[][] board, String word, int index){
         if(index == word.length()){
             return true;
         }
         if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(index)) return false;
 
+
         char temp = board[i][j];
         board[i][j] = ' ';
-        boolean ans = Func(i + 1, j, board, word, index + 1) || Func(i -1, j, board, word, index + 1) ||Func(i , j+1, board, word, index + 1) ||Func(i , j-1, board, word, index + 1);
-        board[i][j] = temp;
-        return ans;
+       boolean ans = WordFinder(i, j + 1, board, word, index + 1) || WordFinder(i, j-1,board,word, index + 1) || WordFinder(i-1,j,board,word, index + 1) || WordFinder(i+1,j,board,word, index + 1);
+       board[i][j] = temp;
+       return ans;
     }
     public boolean exist(char[][] board, String word) {
         
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
                 if(board[i][j] == word.charAt(0)){
-                    if(Func(i, j, board, word, 0)){
-                        return true;
-                    }
-                    
+                    if(WordFinder(i, j, board, word, 0)) return true;
                 }
             }
         }
