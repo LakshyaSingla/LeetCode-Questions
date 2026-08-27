f4ab52b570190d8cc297cb14ca5217cc25cad1dc
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-08-27 21:12:26 +0530

    Time: 8 ms (7%), Space: 43.6 MB (5.94%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,8 +1,7 @@
 class Solution {
-     boolean isValid(int row, int col, List<String> board){
+    boolean isValid(int row, int col, List<String> board){
         int r = row;
         int c = col;
-
         while(r >= 0){
             if(board.get(r).charAt(c) == 'Q'){
                 return false;
@@ -10,37 +9,32 @@ class Solution {
             r--;
         }
         r = row;
-        while(r >=0 && c >= 0){
-            if(board.get(r).charAt(c) == 'Q'){
-                return false;
-            }
+        while(r >= 0 && c >= 0){
+            if(board.get(r).charAt(c) == 'Q') return false;
             r--;
             c--;
-
         }
         r = row;
         c = col;
-        while(r >= 0 && c < board.get(0).length()){
-            if(board.get(r).charAt(c) == 'Q'){
-                return false;
-            }
+        while(r >= 0 && c < board.size()){
+            if(board.get(r).charAt(c) == 'Q') return false;
             r--;
             c++;
         }
         return true;
     }
-    int FindBoard(int row,List<String> board, List<List<String>> ans){
-        if(row == board.size()){
-            ans.add(new ArrayList<>(board));
+    int PossibleBoards(int row, int n, List<String> board){
+        int count = 0;
+        if(row == n){
+            
             return 1;
         }
-        int count = 0;
-        for(int col = 0; col < board.get(0).length(); col++){
+        for(int col = 0; col < n; col++){
             if(isValid(row, col, board)){
                 char[] arr = board.get(row).toCharArray();
                 arr[col] = 'Q';
                 board.set(row, new String(arr));
-                count += FindBoard(row + 1, board,ans);
+                count+=  PossibleBoards(row + 1, n, board);
                 arr[col] = '.';
                 board.set(row, new String(arr));
             }
@@ -48,16 +42,15 @@ class Solution {
         return count;
     }
     public int totalNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
         List<String> board = new ArrayList<>();
         String temp = "";
         for(int i = 0; i < n; i++){
-            temp+= '.';
+            temp+='.';
         }
         for(int i = 0; i < n; i++){
             board.add(temp);
         }
-        return FindBoard(0, board, ans);
+        return PossibleBoards(0, n, board);
         
     }
 }
