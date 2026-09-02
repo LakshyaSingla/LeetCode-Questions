ab5b764c6527376944f60c97cab57096ef288a10
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-09-02 11:47:54 +0530

    Time: 2 ms (25.26%), Space: 47.3 MB (19.76%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,40 +1,41 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int n = intervals.length;
-        List<List<Integer>> ls = new ArrayList<>();
         int i = 0;
-
+        List<List<Integer>> res = new ArrayList<>();
         while(i < n && intervals[i][1] < newInterval[0]){
             List<Integer> temp = new ArrayList<>();
             temp.add(intervals[i][0]);
             temp.add(intervals[i][1]);
-            ls.add(new ArrayList<>(temp));
+            res.add(new ArrayList<>(temp));
             i++;
         }
-
-        while(i < n && intervals[i][0] <= newInterval[1]){
+        while(i < n && newInterval[1] >= intervals[i][0]){
             newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
             newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             i++;
         }
-         List<Integer> merge = new ArrayList<>();
-        merge.add(newInterval[0]);
-        merge.add(newInterval[1]);
-        ls.add(merge);
+        List<Integer> insert = new ArrayList<>();
+        insert.add(newInterval[0]);
+        insert.add(newInterval[1]);
+        res.add(insert);
 
         while(i < n){
-             List<Integer> temp = new ArrayList<>();
-             temp.add(intervals[i][0]);
-             temp.add(intervals[i][1]);
-             ls.add(new ArrayList<>(temp));
-             i++;
+            List<Integer> temp = new ArrayList<>();
+            temp.add(intervals[i][0]);
+            temp.add(intervals[i][1]);
+            res.add(new ArrayList<>(temp));
+            i++;
         }
-        int[][] ans = new int[ls.size()][2];
-        for(int k = 0; k < ans.length; k++){
-            ans[k][0] = ls.get(k).get(0);
-            ans[k][1] = ls.get(k).get(1);
+
+        int[][] ans= new int[res.size()][2];
+        
+        
+        for(int j = 0; j < res.size(); j++){
+            ans[j][0] = res.get(j).get(0);
+            ans[j][1] = res.get(j).get(1);
         }
         return ans;
-        
+
     }
 }
