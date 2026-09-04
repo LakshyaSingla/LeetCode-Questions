cc686530ba26ef0b49c0926948b3cae4be4c7059
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-09-04 22:51:04 +0530

    Time: 54 ms (66.23%), Space: 68.8 MB (83.96%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,10 +1,12 @@
 class Solution {
     public int totalFruit(int[] fruits) {
         int n = fruits.length;
-        int l = 0, r = 0, maxlen = 0;
+        int maxlen = 0;
+        int l = 0, r =0;
         HashMap<Integer, Integer> mpp = new HashMap<>();
         while(r < n){
             mpp.put(fruits[r], mpp.getOrDefault(fruits[r], 0) + 1);
+
             if(mpp.size() > 2){
                 mpp.put(fruits[l], mpp.get(fruits[l]) - 1);
                 if(mpp.get(fruits[l]) == 0){
@@ -14,7 +16,6 @@ class Solution {
             }
             if(mpp.size() <= 2){
                 maxlen = Math.max(maxlen, r - l + 1);
-
             }
             r++;
         }
