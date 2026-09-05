e9645f032710df3b16b7f5c10d4c990fcd419a38
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-09-05 18:47:20 +0530

    Time: 2 ms (99.78%), Space: 45.5 MB (75.18%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,22 +1,21 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int sIndex = -1, minlen = Integer.MAX_VALUE, count = 0;
         int n = s.length();
         int m = t.length();
         int[] hash = new int[256];
         for(int i = 0; i < m; i++){
             hash[t.charAt(i)]++;
         }
-        int l = 0, r = 0;
+        int l = 0, r = 0, count = 0, sIndex = -1, minlen = Integer.MAX_VALUE;
+
         while(r < n){
             if(hash[s.charAt(r)] > 0){
                 count++;
             }
             hash[s.charAt(r)]--;
-
             while(count == m){
                 if(r - l + 1 < minlen){
-                    minlen = r - l + 1;
+                    minlen = r- l + 1;
                     sIndex = l;
                 }
                 hash[s.charAt(l)]++;
@@ -28,6 +27,5 @@ class Solution {
             r++;
         }
         return (sIndex == -1) ? "" : s.substring(sIndex, sIndex + minlen);
-
     }
 }
