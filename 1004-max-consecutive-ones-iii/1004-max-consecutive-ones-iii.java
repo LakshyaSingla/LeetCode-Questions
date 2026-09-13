db627723f51b062b4ac0b8dbe3314ffcd1e5594b
Lakshya Singla <76520519+LakshyaSingla@users.noreply.github.com>
2026-09-13 21:36:36 +0530

    Time: 4 ms (44.59%), Space: 52.3 MB (35.69%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -5,7 +5,6 @@ class Solution {
 
         while(r < nums.length){
             if(nums[r] == 0) zero++;
-            
             if(zero > k){
                 if(nums[l] == 0) zero--;
                 l++;
